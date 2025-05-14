fd7496eb3eeaa63e94ec92b0740db54f6d2c004b
Amet <162996755+Ameteh@users.noreply.github.com>
2025-05-14 22:14:32 +0700

    Update Product.java

--- src/main/java/com/mycompany/mavenproject3/Product.java
+++ src/main/java/com/mycompany/mavenproject3/Product.java
@@ -1,8 +1,8 @@
+package com.example;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.mavenproject3;
 
 /**
  *
